f798dc98eeed6847158656875be89d05634243e9
Norma Lopez <norma_l304@yahoo.com>
2020-03-18 13:30:57 -0600

    Add restaurant class

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,13 @@
+class Restaurant
+  attr_reader :opening_time, :name
 
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
+
+  def dishes
+    dish_set =[]
+  end
+end
